5e66a31c94ac48ec1fa5edaac494253d85f5c89e
Elliott Rezny <erezny@gmail.com>
2015-02-28 07:58:08 -0800

    public configs

--- config/env/test.js
+++ config/env/test.js
@@ -7,7 +7,7 @@ module.exports = {
       access_token_secret:  ''
     },
     controller: {
-      url: 'mongodb://localhost:27017/viewer-dev',
+      url: 'mongodb://localhost:27017/twitter-users',
     },
   },
 
