9fe39dc4a8ec4fd81d75d385b97c5141a517980f
Elliott Rezny <erezny@gmail.com>
2015-02-28 07:24:16 -0800

    Set public release config values

---
+++ config/env/test.js
@@ -0,0 +1,27 @@
+module.exports = {
+  twitter: {
+    api: {
+      consumer_key:         '',
+      consumer_secret:      '',
+      access_token:         '',
+      access_token_secret:  ''
+    },
+    controller: {
+      url: 'mongodb://localhost:27017/viewer-dev',
+    },
+  },
+
+  logger:{
+    level: 'debug',
+    root: './',
+  },
+  keen: {
+    projectId: '',
+    writeKey: '',
+    readKey: '',
+    masterKey: '',
+    protocol: 'https',              // String (optional: https | http | auto)
+    host: 'api.keen.io/3.0',        // String (optional)
+    requestType: 'jsonp'            // String (optional: jsonp, xhr, beacon)
+  },
+};
