6c287e4f943216430a88bda86ac0bf24d3767434
Elliott Rezny <erezny@gmail.com>
2015-03-02 17:49:53 -0800

    test branch values

--- config/env/dev.js
+++ config/env/dev.js
@@ -1,10 +1,10 @@
 module.exports = {
   twitter: {
     api: {
-      consumer_key:         '',
-      consumer_secret:      '',
-      access_token:         '',
-      access_token_secret:  ''
+      consumer_key:         'nqcvYQFZRHRqgerM9F5I7ycft',
+      consumer_secret:      'jOCoMqvwdqLm035ObYXgwLAG6icSPgMe4m6o63vxn9E0kTKHjR',
+      access_token:         '16876313-GnG6mx61biAP35pkSZPeUffPB9h1rBhNm0wMzMFOh',
+      access_token_secret:  'TVYwy47RkyrA2aa4hpVBX39cTyfeHGgXFRCeytnhIaC1Y'
     },
     controller: {
       url: 'mongodb://localhost:27017/twitter',
