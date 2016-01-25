64c793719dd99a6f9448bdb210e9de5327497671
Elliott Rezny <erezny@gmail.com>
2016-01-25 13:20:09 -0600

    sufficiently dockerized. Will be buggy though

--- config/env/prod.js
+++ config/env/prod.js
@@ -3,13 +3,13 @@ var util = require('util');
 module.exports = {
   twitter: {
     api: {
-      consumer_key:         '',
-      consumer_secret:      '',
-      access_token:         '',
-      access_token_secret:  ''
+      consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+      access_token:         process.env.TWITTER_ACCESS_TOKEN,
+      access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
     },
     controller: {
-      url: util.format('mongodb://%s:%d/%s',
+      url: util.format('mongodb://%s:%s/%s',
         process.env.MONGO_TUTUM_SERVICE_HOSTNAME,
         process.env.MONGO_PORT_27017_TCP_PORT,
         process.env.MONGO_COLLECTION
