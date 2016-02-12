af65752eb71172fb247a44e37aab1ae4a09f8d19
Elliott Rezny <erezny@gmail.com>
2016-02-12 16:40:02 -0600

    new folder location for services

---
+++ services/twitter/checkNeo4jFollowers.js
@@ -0,0 +1,237 @@
+
+var util = require('util');
+var RSVP = require('rsvp');
+var test = require('assert');
+var RateLimiter = require('limiter').RateLimiter;
+var Queue = require('bull');
+var assert = require('assert');
+const crow = require("crow-metrics");
+const request = require("request");
+
+var redis = require("redis").createClient({
+  host: process.env.REDIS_HOST,
+  port: parseInt(process.env.REDIS_PORT),
+});
+
+const metrics = new crow.MetricsRegistry({ period: 15000, separator: "." }).withPrefix("mongoToNeo4j.followers");
+
+crow.exportInflux(metrics, request, { url: util.format("%s://%s:%s@%s:%d/write?db=%s",
+  process.env.INFLUX_PROTOCOL, process.env.INFLUX_USERNAME, process.env.INFLUX_PASSWORD,
+  process.env.INFLUX_HOST, parseInt(process.env.INFLUX_PORT), process.env.INFLUX_DATABASE)
+});
+
+metrics.setGauge("heap_used", function () { return process.memoryUsage().heapUsed; });
+metrics.setGauge("heap_total", function () { return process.memoryUsage().heapTotal; });
+metrics.counter("app_started").increment();
+
+var neo4j = require('seraph')( {
+  server: util.format("%s://%s:%s",
+    process.env.NEO4J_PROTOCOL,
+    process.env.NEO4J_HOST,
+    process.env.NEO4J_PORT),
+  endpoint: 'db/data',
+  user: process.env.NEO4J_USERNAME,
+  pass: process.env.NEO4J_PASSWORD });
+
+var MongoClient = require('mongodb').MongoClient,
+  test = require('assert');
+
+var twitterCollection  = null;
+
+var logger = require('tracer').colorConsole( {
+  level: 'info'
+} );
+
+var db = null;
+
+//runtime loop
+MongoClient.connect(util.format('mongodb://%s:%s@%s:%d/%s?authMechanism=SCRAM-SHA-1&authSource=%s',
+  process.env.MONGO_USER,
+  process.env.MONGO_PASSWD,
+  process.env.MONGO_HOST,
+  process.env.MONGO_PORT,
+  process.env.MONGO_DATABASE,
+  process.env.MONGO_DATABASE
+),
+  {
+    logger: logger,
+    numberOfRetries: 10,
+    retryMiliSeconds: 10000
+  },
+  function(err, db_) {
+    if (err){
+      process.exit;
+    }
+  db = db_;
+
+  var openQueries = 0;
+  var queryLimit = 1;
+  var total = 0;
+  var finished = 0;
+
+  logger.trace("connected to mongo");
+
+  var cursor = db.collection("twitterUsers")
+  .find({ 'followers.1': { $exists: true } })
+  .project({
+      id_str: 1,
+      screen_name: 1,
+      followers: 1,
+  })
+
+  var stream = cursor.stream();
+
+  stream.on('end', function() {
+    logger.info("End of mongo stream");
+    //db.close();
+    setTimeout(function() {
+      process.exit();
+    }, 60 * 1000);
+  });
+
+  cursor.count(function(err, count) {
+    logger.info("Number of users %d", count);
+    total = count;
+  });
+
+  stream.on('data', function(user) {
+    openQueries++;
+    if ( openQueries >= queryLimit) {
+      stream.pause();
+    }
+    logger.trace('user from mongo: %s', user.screen_name);
+    metrics.counter("users_loaded").increment();
+
+    redis.hgetall(util.format("twitter:%s",user.id_str), function(err, obj) {
+        if (obj && obj.neo4jID){
+          updateFollowers({ id_str: user.id_str, screen_name: user.screen_name, neo4jID: obj.neo4jID, followers: user.followers })
+          .then(function(results) {
+            logger.trace("relationship Results: %j", results);
+            db.collection("twitterUsers").findOneAndUpdate(
+              { id_str: user.id_str },
+              { $set: { followers: results, "identifiers.neo4j": obj.neo4jID },
+                $inc: { 'import.neo4j.friends': 1 } },
+              { projection: { id_str: 1, screen_name: 1 } } ).then(function(result) {
+                logger.info("completed %s", user.screen_name);
+                metrics.counter("users_saved").increment();
+                restartQueries();
+              }, function(err) {
+                  logger.err("mongo error saving %s", user.screen_name);
+                  restartQueries();
+              });
+          }, function(err) {
+            logger.error("relationship error: %j", err);
+            restartQueries();
+          });
+        } else {
+          logger.debug("user not in neo4j yet %s", user.screen_name);
+          metrics.counter("users_not_exist").increment();
+          restartQueries();
+        }
+    });
+
+  });
+
+  function restartQueries(){
+    finished++;
+    openQueries--;
+    metrics.counter("users_finished").increment();
+    if (openQueries < queryLimit ) {
+      stream.resume();
+    }
+    if (finished % 10 == 0){
+      logger.info("completed %d / %d", finished, total);
+    }
+  }
+
+});
+
+//requires user.id_str, user.followers = []
+function updateFollowers(user){
+  assert( typeof(user.neo4jID) == "string" );
+  assert( typeof(user.followers) == "object" );
+  return new RSVP.Promise( function (resolve, reject) {
+    var followersTasks = [];
+    for (otherUser of user.followers){
+      followersTasks.push(upsertFollowerIfExists(user, otherUser));
+    }
+    logger.info("updateFollowers %s: %d", user.screen_name, followersTasks.length);
+    RSVP.allSettled(followersTasks).then(function(results) {
+      logger.debug("updateFollowers all settled %s", user.screen_name);
+      var followersNotFound = [];
+      for (result of results) {
+        if (result.state == "rejected") {
+          followersNotFound.push(result.reason);
+        }
+      }
+      logger.info("updateFollowers %s Remaining: %d", user.screen_name, followersNotFound.length);
+      resolve(followersNotFound);
+    })
+  });
+}
+
+function upsertFollowerIfExists(user, follower){
+  assert( typeof(user.neo4jID) == "string" );
+  assert( typeof(user.id_str) == "string" );
+  assert( typeof(follower) == "string" );
+  return new RSVP.Promise( function (resolve, reject) {
+    redis.hgetall(util.format("twitter:%s",follower), function(err, obj) {
+      if (obj && obj.neo4jID){
+        upsertRelationship({ id: obj.neo4jID }, { id: user.neo4jID }).then(function(rel) {
+          resolve(follower);
+        }, function(err) {
+          reject(follower);
+        });
+      } else {
+        metrics.counter("rel_user_not_exist").increment();
+        reject(follower);
+      }
+    });
+  });
+}
+
+var sem = require('semaphore')(2);
+
+function upsertRelationship(node, friend) {
+  assert( typeof(node.id) == "string" );
+  assert( typeof(friend.id) == "string" );
+  return new RSVP.Promise( function (resolve, reject) {
+    sem.take(function() {
+      neo4j.query("start x=node({idx}), n=node({idn}) MATCH (x)-[r:follows]->(n) RETURN r",
+        { idx: node.id, idn: friend.id }, function(err, rels) {
+        if (err){
+          logger.error("neo4j find error %j",err);
+          metrics.counter("rel_find_error").increment();
+          reject("error");
+          sem.leave();
+          return;
+        }
+        if (rels.length > 0) {
+          for (rel of rels){
+            if (rel.end == friend.id){
+              //TODO search for duplicates and remove duplicates
+              logger.trace("relationship found %j", rel);
+              metrics.counter("rel_already_exists").increment();
+              resolve(rel);
+              sem.leave();
+              return;
+            }
+          }
+        }
+        neo4j.relate(node.id, 'follows', friend.id, function(err, rel) {
+          if (err){
+            logger.error("neo4j save error %j",err);
+            metrics.counter("rel_save_error").increment();
+            reject("error");
+            sem.leave();
+            return;
+          }
+          logger.debug("saved relationship %j", rel);
+          metrics.counter("rel_saved").increment();
+          sem.leave();
+          resolve(rel);
+        });
+      })
+    });
+  });
+}
---
+++ services/twitter/checkNeo4jUsers.js
@@ -0,0 +1,185 @@
+
+var util = require('util');
+var RSVP = require('rsvp');
+var test = require('assert');
+var RateLimiter = require('limiter').RateLimiter;
+var Queue = require('bull');
+var assert = require('assert');
+const crow = require("crow-metrics");
+const request = require("request");
+
+var redis = require("redis").createClient({
+  host: process.env.REDIS_HOST,
+  port: parseInt(process.env.REDIS_PORT),
+});
+
+const metrics = new crow.MetricsRegistry({ period: 15000, separator: "." }).withPrefix("mongoToNeo4j.users");
+
+crow.exportInflux(metrics, request, { url: util.format("%s://%s:%s@%s:%d/write?db=%s",
+  process.env.INFLUX_PROTOCOL, process.env.INFLUX_USERNAME, process.env.INFLUX_PASSWORD,
+  process.env.INFLUX_HOST, parseInt(process.env.INFLUX_PORT), process.env.INFLUX_DATABASE)
+});
+
+metrics.setGauge("heap_used", function () { return process.memoryUsage().heapUsed; });
+metrics.setGauge("heap_total", function () { return process.memoryUsage().heapTotal; });
+metrics.counter("app_started").increment();
+
+var neo4j = require('seraph')( {
+  server: util.format("%s://%s:%s",
+    process.env.NEO4J_PROTOCOL,
+    process.env.NEO4J_HOST,
+    process.env.NEO4J_PORT),
+  endpoint: 'db/data',
+  user: process.env.NEO4J_USERNAME,
+  pass: process.env.NEO4J_PASSWORD });
+
+var MongoClient = require('mongodb').MongoClient,
+  test = require('assert');
+
+var twitterCollection  = null;
+
+var logger = require('tracer').colorConsole( {
+  level: 'info'
+} );
+
+var db = null;
+
+//runtime loop
+MongoClient.connect(util.format('mongodb://%s:%s@%s:%d/%s?authMechanism=SCRAM-SHA-1&authSource=%s',
+  process.env.MONGO_USER,
+  process.env.MONGO_PASSWD,
+  process.env.MONGO_HOST,
+  process.env.MONGO_PORT,
+  process.env.MONGO_DATABASE,
+  process.env.MONGO_DATABASE
+),
+  {
+    logger: logger,
+    numberOfRetries: 10,
+    retryMiliSeconds: 10000
+  },
+  function(err, db_) {
+    if (err){
+      process.exit;
+    }
+  db = db_;
+
+  logger.trace("connected to mongo");
+
+  var cursor = db.collection("twitterUsers")
+  .find( { "identifiers.neo4j": { $exists: false } })
+  .project({
+      id_str: 1,
+      screen_name: 1,
+      name: 1,
+      followers_count: 1,
+      friends_count: 1,
+      favourites_count: 1,
+      description: 1,
+      location: 1,
+      statuses_count: 1
+  })
+
+  var stream = cursor.stream();
+
+  stream.on('end', function() {
+    logger.info("End of mongo stream");
+    //db.close();
+  });
+
+  var openQueries = 0;
+  var queryLimit = 20;
+  var total = 0;
+  var finished = 0;
+
+  cursor.count(function(err, count) {
+    logger.info("Number of users %d", count);
+    total = count;
+  });
+
+  stream.on('data', function(user) {
+    openQueries++;
+    if ( openQueries > queryLimit) {
+      stream.pause();
+    }
+    logger.trace('user from mongo: %s', user.screen_name);
+
+    redis.hgetall(util.format("twitter:%s",user.id_str), function(err, obj) {
+
+        if (obj && obj.neo4jID){
+        metrics.counter("user_exists").increment();
+        restartQueries();
+        } else {
+          upsertNodeToNeo4j(user)
+            .then( function(user) {
+              logger.debug('next user');
+              metrics.counter("user_saved").increment();
+              restartQueries()
+            }, function(err) {
+              logger.error('userByIDQueue err %j', err);
+              metrics.counter("user_error").increment();
+              restartQueries()
+          });
+        }
+    });
+
+  });
+
+  function restartQueries(){
+    finished++;
+    openQueries--;
+    if (openQueries < queryLimit / 2 ) {
+      stream.resume();
+    }
+    if (finished % 100 == 0){
+      logger.info("completed %d / %d", finished, total);
+    }
+  }
+
+});
+
+var sem = require('semaphore')(5);
+
+function upsertNodeToNeo4j(node) {
+  delete node.id;
+  return new RSVP.Promise( function (resolve, reject) {
+    logger.trace('upserting %s %s', node.screen_name, node.id_str);
+    sem.take(function() {
+    neo4j.find( { id_str: node.id_str }, false, "twitterUser" ,
+        function(err, results) {
+      if (err){
+        logger.error("neo4j find %s %j",node.screen_name, err);
+        metrics.counter("neo4j_find_error").increment();
+        reject(err);
+        sem.leave();
+        return;
+      }
+
+      if (results.length > 0){
+        logger.debug("found %j", results);
+        //if results.length > 1, flag as error
+        redis.hset(util.format("twitter:%s",node.id_str), "neo4jID", results.id, function(err, res) { });
+        metrics.counter("neo4j_exists").increment();
+        resolve(node);
+        sem.leave();
+        return;
+      }
+
+      neo4j.save(node, "twitterUser", function(err, savedNode) {
+        if (err){
+          logger.error("neo4j save %s %j", node.screen_name, err);
+          metrics.counter("neo4j_save_error").increment();
+          reject(err);
+          sem.leave();
+          return;
+        }
+        redis.hset(util.format("twitter:%s",node.id_str), "neo4jID", savedNode.id, function(err, res) { });
+        logger.debug('inserted user %s', savedNode.screen_name);
+        metrics.counter("neo4j_inserted").increment();
+        sem.leave();
+        resolve(savedNode);
+      });
+    });
+  });
+  });
+}
