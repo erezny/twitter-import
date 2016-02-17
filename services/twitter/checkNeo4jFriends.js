eb975f82ea6789dafdc3f4db67b17aa5878345e5
Elliott Rezny <erezny@gmail.com>
2016-02-17 11:21:06 -0600

    changing queue sorts

--- services/twitter/checkNeo4jFriends.js
+++ services/twitter/checkNeo4jFriends.js
@@ -82,7 +82,8 @@ MongoClient.connect(util.format('mongodb://%s:%s@%s:%d/%s?authMechanism=SCRAM-SH
       screen_name: 1,
       friends: 1,
       'identifiers.neo4jID': 1,
-  });
+      friends_count: 1
+  }).sort({ friends_count: 1 });
 
   var stream = cursor.stream();
 
