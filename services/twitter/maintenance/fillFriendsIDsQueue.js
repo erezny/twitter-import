a7cd86f214654a006c709269324c07ee28ec3537
Elliott Rezny <erezny@gmail.com>
2016-02-23 21:47:54 -0600

    fixes

--- services/twitter/maintenance/fillFriendsIDsQueue.js
+++ services/twitter/maintenance/fillFriendsIDsQueue.js
@@ -26,7 +26,7 @@ var neo4j = require('seraph')( {
   user: process.env.NEO4J_USERNAME,
   pass: process.env.NEO4J_PASSWORD });
 
-neo4j.queryRaw("match (n:twitterUser) where n.friends_count > 0 with n limit 10000 " +
+neo4j.queryRaw("match (n:twitterUser) where n.friends_count > 0 and n.friends_count < 100000 with n limit 10000 " +
   "match p=(n)-[:follows]->(:twitterUser) " +
   "WITH n, count(p) AS friends, count(p)/n.friends_count as ratio " +
   "return n order by ratio limit 10", function(err, results) {
@@ -37,9 +37,7 @@ neo4j.queryRaw("match (n:twitterUser) where n.friends_count > 0 with n limit 100
   }
   logger.trace("neo4j found %j", results);
   for ( users of results.data) {
-    logger.debug("node found %s %s", users[0].data.screen_name, users[0].data.id_str);
-    //queue.create('queryFriendsIDs', { user: users[0].data, cursor: "-1" } ).attempts(5).removeOnComplete( true ).save();
-
+    logger.debug("pushing %s %s", users[0].data.screen_name, users[0].data.id_str);
+    queue.create('queryFriendsIDs', { user: users[0].data, cursor: "-1" } ).attempts(5).removeOnComplete( true ).save();
   }
-
 })
