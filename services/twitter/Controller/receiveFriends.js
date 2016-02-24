b87ce0ddd69ec6fa1df10b91c90f1655f55d5f31
Elliott Rezny <erezny@gmail.com>
2016-02-24 15:09:26 -0600

    performance

--- services/twitter/Controller/receiveFriends.js
+++ services/twitter/Controller/receiveFriends.js
@@ -128,7 +128,7 @@ function lookupNeo4jID(user){
   });
 }
 
-queue.process('receiveFriend', 40, receiveFriend );
+queue.process('receiveFriend', 10, receiveFriend );
 
 var metricRelFindError = metrics.counter("rel_find_error");
 var metricRelAlreadyExists = metrics.counter("rel_already_exists");
