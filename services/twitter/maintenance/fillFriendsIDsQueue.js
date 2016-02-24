e94e6eeebe7f58bbc4eb943e06b0c183a315b970
Elliott Rezny <erezny@gmail.com>
2016-02-23 21:44:19 -0600

    script to push a few users onto the friends api query queue

---
+++ services/twitter/maintenance/fillFriendsIDsQueue.js
@@ -0,0 +1,45 @@
+
+// #refactor:10 write queries
+var util = require('util');
+var assert = require('assert');
+
+var RSVP = require('rsvp');
+var logger = require('tracer').colorConsole( {
+  level: 'debug'
+} );
+var kue = require('kue');
+var queue = kue.createQueue({
+  prefix: 'twitter',
+  redis: {
+    port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST,
+    db: 1, // if provided select a non-default redis db
+  }
+});
+
+var neo4j = require('seraph')( {
+  server: util.format("%s://%s:%s",
+  process.env.NEO4J_PROTOCOL,
+  process.env.NEO4J_HOST,
+  process.env.NEO4J_PORT),
+  endpoint: 'db/data',
+  user: process.env.NEO4J_USERNAME,
+  pass: process.env.NEO4J_PASSWORD });
+
+neo4j.queryRaw("match (n:twitterUser) where n.friends_count > 0 with n limit 10000 " +
+  "match p=(n)-[:follows]->(:twitterUser) " +
+  "WITH n, count(p) AS friends, count(p)/n.friends_count as ratio " +
+  "return n order by ratio limit 10", function(err, results) {
+  if (err){
+    logger.error("neo4j find error %j",err);
+    reject("error");
+    return;
+  }
+  logger.trace("neo4j found %j", results);
+  for ( users of results.data) {
+    logger.debug("node found %s %s", users[0].data.screen_name, users[0].data.id_str);
+    //queue.create('queryFriendsIDs', { user: users[0].data, cursor: "-1" } ).attempts(5).removeOnComplete( true ).save();
+
+  }
+
+})
