19514bb031967a553474b07382e7b6038e0b5e00
Elliott Rezny <erezny@gmail.com>
2016-02-25 22:56:01 -0600

    add a little bit of randomness to offset the current 10 hour delay in inserting relationships

--- services/twitter/maintenance/fillFriendsIDsQueue.js
+++ services/twitter/maintenance/fillFriendsIDsQueue.js
@@ -40,7 +40,7 @@ function checkfillFriendsIDs() {
 
 function queryTemplate(sortDir){
   return util.format("match (service{type:\"VIP\"})--()--(n:twitterUser) " +
-    "with distinct n as n " +
+    "with distinct n as n, rand() as r order by r limit 100" +
     "match p=(n)-[:follows]->(:twitterUser) " +
     "WITH n, count(p) AS friends, n.friends_count - count(p) as remaining " +
     "return n order by remaining %s limit 5", sortDir);
