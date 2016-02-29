baa93081b8942c9364a56e8104d9c0a60371c6dc
Elliott Rezny <erezny@gmail.com>
2016-02-28 20:30:13 -0600

    typo

--- services/twitter/Controller/receiveFriends.js
+++ services/twitter/Controller/receiveFriends.js
@@ -123,50 +123,52 @@ function upsertStubUserToNeo4j(user) {
   delete user.id;
   return new RSVP.Promise( function (resolve, reject) {
     logger.trace('Checking stub user %s', user.id_str);
-    neo4j.find( { id_str: user.id_str }, false, "twitterUser" ,
-        function(err, results) {
-      if (err){
-        userSem.leave();
-        logger.error("neo4j find %s %j",user.screen_name, err);
-        metrics.counter("neo4j_find_error").increment();
-        reject({ err:err, reason:"neo4j find user error" });
-        return;
-      }
-      if (results.length > 0){
-        userSem.leave();
-        logger.debug("found %j", results);
-        //if results.length > 1, flag as error
-        redis.hset(util.format("twitter:%s",user.id_str), "neo4jID", results[0].id, function(err, res) { });
-        metrics.counter("neo4j_exists").increment();
-        resolve(user);
-        return;
-      }
-      logger.debug('stubbing user %s', user.id_str);
-      neo4j.save(user, function(err, savedUser) {
+    userSem.take(function() {
+      neo4j.find( { id_str: user.id_str }, false, "twitterUser" ,
+          function(err, results) {
         if (err){
           userSem.leave();
-          logger.error("neo4j save %s %j", user.id_str, err);
-          metrics.counter("neo4j_save_error").increment();
-          reject({ err:err, reason:"neo4j save user error" });
+          logger.error("neo4j find %s %j",user.screen_name, err);
+          metrics.counter("neo4j_find_error").increment();
+          reject({ err:err, reason:"neo4j find user error" });
           return;
         }
-        logger.debug('inserted user %s', savedUser.id_str);
-        neo4j.label(savedUser, "twitterUser", function(err, labeledUser) {
+        if (results.length > 0){
           userSem.leave();
+          logger.debug("found %j", results);
+          //if results.length > 1, flag as error
+          redis.hset(util.format("twitter:%s",user.id_str), "neo4jID", results[0].id, function(err, res) { });
+          metrics.counter("neo4j_exists").increment();
+          resolve(user);
+          return;
+        }
+        logger.debug('stubbing user %s', user.id_str);
+        neo4j.save(user, function(err, savedUser) {
           if (err){
-            logger.error("neo4j label error %s %j", user.id_str, err);
-            metrics.counter("neo4j_label_error").increment();
-            reject({ err:err, reason:"neo4j label user error" });
+            userSem.leave();
+            logger.error("neo4j save %s %j", user.id_str, err);
+            metrics.counter("neo4j_save_error").increment();
+            reject({ err:err, reason:"neo4j save user error" });
             return;
           }
-          redis.hset(util.format("twitter:%s",savedUser.id_str), "neo4jID", savedUser.id, function(err, res) {
-            logger.debug('labeled user %s', savedUser.id_str);
-            metrics.counter("neo4j_inserted").increment();
-            resolve(savedUser);
-          });
+          logger.debug('inserted user %s', savedUser.id_str);
+          neo4j.label(savedUser, "twitterUser", function(err, labeledUser) {
+            userSem.leave();
+            if (err){
+              logger.error("neo4j label error %s %j", user.id_str, err);
+              metrics.counter("neo4j_label_error").increment();
+              reject({ err:err, reason:"neo4j label user error" });
+              return;
+            }
+            redis.hset(util.format("twitter:%s",savedUser.id_str), "neo4jID", savedUser.id, function(err, res) {
+              logger.debug('labeled user %s', savedUser.id_str);
+              metrics.counter("neo4j_inserted").increment();
+              resolve(savedUser);
+            });
 
+          });
         });
       });
     });
-  });
+  })
 }
