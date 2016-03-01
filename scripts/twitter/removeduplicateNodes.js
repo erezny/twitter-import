d3e79d33ed33b48c46e27ad41aa251e92dd0cf36
Elliott Rezny <erezny@gmail.com>
2016-03-01 15:07:44 -0600

    meh

--- scripts/twitter/removeduplicateNodes.js
+++ scripts/twitter/removeduplicateNodes.js
@@ -95,7 +95,7 @@ function scanUser(key){
   });
 }
 
-var sem = require('semaphore')(2);
+var sem = require('semaphore')(1);
 function removeDuplicates(id_str, neo4jID) {
     return new RSVP.Promise( function (resolve, reject) {
       sem.take(function() {
@@ -128,7 +128,7 @@ function removeDuplicates(id_str, neo4jID) {
     });
 }
 
-var removeSem = require('semaphore')(2);
+var removeSem = require('semaphore')(1);
 function removeNode(id) {
     return new RSVP.Promise( function (resolve, reject) {
       removeSem.take(function() {
