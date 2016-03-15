2ddc0fde27247d21fe143804448f29cbcf8a9c92
Elliott Rezny <erezny@gmail.com>
2016-03-15 15:24:28 -0500

    let neo4j handle node check/create

--- services/twitter/Controller/receiveFriends.js
+++ services/twitter/Controller/receiveFriends.js
@@ -19,47 +19,22 @@ var redis = require("redis").createClient({
   host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT),
 });
-var redisKey = function(user) { return util.format("twitter:%s", user.id_str); };
-var redisRelKey = function(rel) { return util.format("twitter-friend:%s:%s", rel.user.id_str, rel.friend.id_str) };
 
-setInterval( function() {
+function count_queue(){
   queue.inactiveCount( 'receiveFriend', function( err, total ) { // others are activeCount, completeCount, failedCount, delayedCount
     metrics.setGauge("queue.inactive", total);
   });
-}, 15 * 1000 );
+};
+setInterval( count_queue, 15 * 1000 );
 
 const metricRelExists = metrics.counter("rel_exists");
 const metricUserNotExist = metrics.counter("user_not_exist");
 const metricFinish = metrics.counter("finish");
 const metricStart = metrics.counter("start");
 const metricError = metrics.counter("error");
-
-function lookupNeo4jID(user, rel){
-  return new RSVP.Promise( function(resolve, reject) {
-    redis.hgetall(redisKey(user), function(err, redisUser) {
-      if (redisUser && redisUser.neo4jID && redisUser.neo4jID != "undefined" && parseInt(redisUser.neo4jID) > 1){
-        resolve({ id: parseInt(redisUser.neo4jID) });
-      } else {
-        queue.create('receiveStubUser', { user: user, rel: rel } ).removeOnComplete( true ).save();
-        metricUserNotExist.increment();
-        reject({ message: "user doesn't exist" });
-      }
-    });
-  });
-}
-
-function lookupRel(rel){
-  return new RSVP.Promise( function(resolve, reject) {
-    lookupNeo4jID(rel.user, rel)
-    .then(function(user) {
-      lookupNeo4jID(rel.friend, rel)
-      .then(function(friend) {
-        queue.create('saveFriend', { user: user, friend: friend } ).removeOnComplete( true ).save();
-        resolve(rel);
-      }, reject );
-    }, reject );
-  });
-}
+const metricTxnFinished = metrics.counter("txnFinished");
+const metricRelSaved = metrics.counter("rel_saved");
+const metricRelError = metrics.counter("rel_error");
 
 function receiveFriend (job, done) {
   logger.trace("received job %j", job);
@@ -67,17 +42,45 @@ function receiveFriend (job, done) {
   var rel = job.data;
 
   function finished (result){
-    return new RSVP.Promise( function (resolve, reject) {
       metricFinish.increment();
       logger.trace("finish");
-      resolve();
-    });
+      done();
   }
 
-  lookupRel(rel)
+  saveFriend(rel)
   .then(finished, finished)
-  .finally(done);
 
 };
 
-queue.process('receiveFriend', 10, receiveFriend );
+queue.process('receiveFriend', 500, receiveFriend );
+
+var txn = neo4j.batch();
+setInterval(function() {
+  var txn_commit = txn;
+  txn = neo4j.batch();
+  txn_commit.commit(function (err, results) {
+    metricTxnFinished.increment();
+  });
+} , 30 * 1000);
+
+const cypher = "merge (x:twitterUser { id_str: {user} }) " +
+            "merge (y:twitterUser { id_str: {friend} }) " +
+            "merge (x)-[r:follows]-(y) ";
+
+function saveFriend(rel) {
+  return new Promise(function(resolve, reject) {
+    var user = rel.user;
+    var friend = rel.friend;
+
+    txn.query(cypher, { user: user.id_str, friend: friend.id_str } , function(err, results) {
+      if (err){
+        metricRelError.increment();
+        reject();
+      } else {
+        metricRelSaved.increment();
+        resolve();
+      }
+    });
+
+  });
+}
