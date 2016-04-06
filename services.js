e40e138f224553969862c924c4e9265b8c1fcd8e
Elliott Rezny <erezny@gmail.com>
2016-04-05 22:15:18 -0500

    stripping down services

--- services.js
+++ services.js
@@ -19,12 +19,12 @@ var childs = {
 //    queryFriendsList: new (forever.Monitor)('./services/twitter/API/queryFriendsList.js', childOpts).start(),
     queryFriendsIDs: new (forever.Monitor)('./services/twitter/API/queryFriendsIDs.js', childOpts).start(),
     queryFollowerIDs: new (forever.Monitor)('./services/twitter/API/queryFollowersIDs.js', childOpts).start(),
-    queryFollowersList: new (forever.Monitor)('./services/twitter/API/queryFollowersList.js', childOpts).start(),
+//    queryFollowersList: new (forever.Monitor)('./services/twitter/API/queryFollowersList.js', childOpts).start(),
 // fillFriendsQueue: new (forever.Monitor)('./services/twitter/maintenance/fillFriendsIDsQueue.js', childOpts).start(),
 // fillFollowersQueue: new (forever.Monitor)('./services/twitter/maintenance/fillFollowersQueue.js', childOpts).start(),
 //    statsVIP: new (forever.Monitor)('./services/twitter/monitoring/statsVIP.js', childOpts).start(),
-    fillUsersQueue: new (forever.Monitor)('./services/twitter/maintenance/fillUsersQueue.js', childOpts).start(),
-    countImportedRelationships: new (forever.Monitor)('./services/twitter/maintenance/countImportedRelationships.js', childOpts).start(),
+//    fillUsersQueue: new (forever.Monitor)('./services/twitter/maintenance/fillUsersQueue.js', childOpts).start(),
+//    countImportedRelationships: new (forever.Monitor)('./services/twitter/maintenance/countImportedRelationships.js', childOpts).start(),
     kueUI: new (forever.Monitor)('./services/twitter/ui/kue.js', childOpts).start()
 };
 function addEvents(child) {
