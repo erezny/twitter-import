5ee8a8fd37343677e95732de1ad5280957c975e1
Elliott Rezny <erezny@gmail.com>
2016-04-12 21:18:19 -0500

    cleanup old indexed property values

--- scripts/twitter/queryNeo4j.js
+++ scripts/twitter/queryNeo4j.js
@@ -1,5 +1,4 @@
 
-// #refactor:10 write queries
 var util = require('util');
 var assert = require('assert');
 
@@ -7,39 +6,46 @@ var RSVP = require('rsvp');
 var logger = require('tracer').colorConsole( {
   level: 'debug'
 } );
-var kue = require('kue');
-var queue = kue.createQueue({
-  prefix: 'twitter',
-  redis: {
-    port: process.env.REDIS_PORT,
-    host: process.env.REDIS_HOST,
-    db: 1, // if provided select a non-default redis db
-  }
-});
+var neo4j = require('../../lib/neo4j.js');
 
-var neo4j = require('seraph')( {
-  server: util.format("%s://%s:%s",
-  process.env.NEO4J_PROTOCOL,
-  process.env.NEO4J_HOST,
-  process.env.NEO4J_PORT),
-  endpoint: 'db/data',
-  user: process.env.NEO4J_USERNAME,
-  pass: process.env.NEO4J_PASSWORD });
-
-neo4j.queryRaw("MATCH (x:twitterUser{screen_name:\"erezny\"})-[r:follows]->(n) RETURN n", function(err, results) {
-  if (err){
-    logger.error("neo4j find error %j",err);
-    reject("error");
-    return;
-  }
-  logger.trace("neo4j found %j", results);
-  for ( users of results.data) {
-    //TODO search for duplicates and remove duplicates
-    if (users[0].data.followers_count < 20000){
-    logger.debug("node found %s %s", users[0].data.screen_name, users[0].data.id_str);
-    queue.create('queryFollowersList', { user: users[0].data, cursor: "-1" } ).attempts(5).removeOnComplete( true ).save();
-
+function deleteProperty(property){
+  return new Promise(function(resolve, reject) {
+    var query = util.format("match (n:twitterUser) " +
+          "using index n:twitterUser(%s) where exists(n.%s) " +
+          "with n limit 500000 remove n.%s return count(*) as num", property, property, property);
+    function runQuery(){
+      return new Promise(function(resolve, reject) {
+        neo4j.queryRaw(query, function(err, results, something) {
+          if (err){
+            logger.error("neo4j find error %j",err);
+            reject("error");
+            return;
+          }
+          logger.info("deleted %d", results.data[0][0]);
+          resolve(results.data[0][0]);
+        });
+      });
     }
-  }
+    runQuery().then(checkRestart);
 
-})
+    function checkRestart(num){
+      if (num > 0){
+        process.nextTick(run);
+      } else {
+        resolve();
+      }
+    }
+
+    function run(){
+        runQuery().then(checkRestart);
+    }
+  });
+}
+
+function deleteProperties(properties){
+  var job;
+  for (var property of properties){
+    //umm
+  }
+}
+deleteProperty("friends_imported_count");
