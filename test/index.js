771bb764672318073ed985d26e2787cb05429e5e
Elliott Rezny <erezny@gmail.com>
2016-04-21 17:36:07 -0500

    add sigint handler to util lib

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
 // http://fredkschott.com/post/2014/05/nodejs-testing-essentials/
 
 require('./t_util');
-require('./twitter');
+//require('./twitter');
--- test/t_util.js
+++ test/t_util.js
@@ -2,19 +2,25 @@
 var util = require('../lib/util.js');
 var assert = require('assert');
 
-describe('util', function(){
+describe('util', function() {
 
-  it('should have uniqArray', function(){
-    //console.log(typeof(util.uniqArray));
+  it('should have uniqArray', function() {
     assert.equal(typeof(util.uniqArray), 'function');
-    //done();
   });
 
-  describe('#uniqArray()', function(){
+  it('should have isEmpty', function() {
+    assert.equal(typeof(util.isEmpty), 'function');
+  });
 
-    it('should return all values of a unique array', function(done){
+  it('should have CatchSigterm', function() {
+    assert.equal(typeof(util.CatchSigterm), 'function');
+  });
 
-      var unique_array = [1,2,3];
+  describe('#uniqArray()', function() {
+
+    it('should return all values of a unique array', function(done) {
+
+      var unique_array = [ 1,2,3 ];
       var result = util.uniqArray(unique_array);
 
       //console.log(result);
@@ -27,10 +33,10 @@ describe('util', function(){
       done();
 
     });
-    
-    it('should return fewer values of a non-unique array', function(done){
 
-      var unique_array = [1,2,3,3];
+    it('should return fewer values of a non-unique array', function(done) {
+
+      var unique_array = [ 1,2,3,3 ];
       var result = util.uniqArray(unique_array);
 
       //console.log(result);
@@ -40,6 +46,53 @@ describe('util', function(){
       done();
 
     });
+  });
+
+  describe('#isEmpty()', function() {
+
+    it('should return true for {}', function(done) {
+
+      assert.equal(true, util.isEmpty({ }));
+      done();
+
+    });
+
+    it('should return false for object with keys', function(done) {
+
+      assert.equal(false, util.isEmpty({ a: 1 }));
+      assert.equal(false, util.isEmpty({ a: null }));
+      assert.equal(false, util.isEmpty([ 1,2,3 ]));
+      done();
+    });
+
+    it('should return true for null', function(done) {
+
+      assert.equal(true, util.isEmpty(null));
+      done();
+
+    });
+  });
+
+  describe('#CatchSigterm()', function() {
+
+    it('should set to true upon construction', function(done) {
+      var keep_running = new util.CatchSigterm();
+      console.log(keep_running);
+      assert.equal(true, keep_running.get());
+      done();
+    });
+
+    it('should catch SIGINT (press ctrl-c) and set to false after SIGINT', function(done) {
+      var keep_running = new util.CatchSigterm();
+      this.timeout(6000);
+      console.log("Press ctrl-c");
+      setInterval(() => {
+        if ( !keep_running.get()){
+          done();
+        }
+      }, 1000);
+    });
+
   });
 
 });
