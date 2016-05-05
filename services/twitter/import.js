eaa5a893a9815b753d2f45fcf9433ca5a122d8e3
Elliott Rezny <erezny@gmail.com>
2016-05-05 13:12:23 -0500

    lookup dynamic VIP node

--- services/twitter/import.js
+++ services/twitter/import.js
@@ -14,14 +14,16 @@ var neo4j = new Neo4j(logger, metrics);
 var serviceHandler = new Services(neo4j, logger, metrics);
 
 serviceHandler.runAllUserImports().then(function(jobs) {
-  for (var job of serviceHandler.runAppImports()){
-    jobs.push(job);
-  }
-  RSVP.allSettled(jobs)
-  .then(function() {
-    process.nextTick(process.exit, 0);
-  }, function(err) {
-    logger.error(err);
+  serviceHandler.runAppImports().then(function(appJobs) {
+    for (var job of appJobs) {
+      jobs.push(job);
+    }
+    RSVP.allSettled(jobs)
+    .then(function() {
       process.nextTick(process.exit, 0);
+    }, function(err) {
+      logger.error(err);
+        process.nextTick(process.exit, 0);
+    });
   });
 });
